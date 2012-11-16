99947e54005253ffebc6cc0399fb96dc3eb25321
Bram Stein <b.l.stein@gmail.com>
2012-11-16 23:12:06 +0100

    Added basic tests for dropzone and for dragenter and dragleave callbacks.

--- test/knockout.dragdrop-test.js
+++ test/knockout.dragdrop-test.js
@@ -7,6 +7,55 @@ function createEvent(type) {
 
 describe('knockout.dragdrop', function () {
     describe('drop', function () {
+        it('throws an exception when the dropzone is incorrectly configured', function () {
+            var el = document.getElementById('bad-drop');
+            expect(function () {
+                ko.applyBindings({}, el);
+            }).to.throwException();
+        });
+
+        it('supports simple configuration', function () {
+            var el = document.getElementById('simple-drop');
+            expect(function () {
+                ko.applyBindings({}, el);
+            }).to.not.throwException();
+        });
+
+        it('supports dragEnter callback', function (done) {
+            var el = document.getElementById('enter-dragdrop'),
+                drop = el.firstChild,
+                drag = el.lastChild;
+
+            ko.applyBindings({
+                dragEnter: function (data, event, zone) {
+                    expect(zone).to.eql('test');
+                    expect(event).to.not.eql(null);
+                    done();
+                }
+            }, el);
+            drag.dispatchEvent(createEvent('dragstart'));
+            drop.dispatchEvent(createEvent('dragenter'));
+            drag.dispatchEvent(createEvent('dragend'));
+        });
+
+        it('supports dragLeave callback', function (done) {
+            var el = document.getElementById('leave-dragdrop'),
+                drop = el.firstChild,
+                drag = el.lastChild;
+
+            ko.applyBindings({
+                dragLeave: function (data, event, zone) {
+                    expect(zone).to.eql('test');
+                    expect(event).to.not.eql(null);
+                    done();
+                }
+            }, el);
+
+            drag.dispatchEvent(createEvent('dragstart'));
+            drop.dispatchEvent(createEvent('dragenter'));
+            drop.dispatchEvent(createEvent('dragleave'));
+            drag.dispatchEvent(createEvent('dragend'));
+        });
     });
 
     describe('drag', function () {
@@ -24,7 +73,7 @@ describe('knockout.dragdrop', function () {
             }).to.not.throwException();
         });
 
-        it('supports dragStart event', function (done) {
+        it('supports dragStart callback', function (done) {
             var el = document.getElementById('start-drag');
             ko.applyBindings({
                 dragStart: function (data, event, zone) {
@@ -36,7 +85,7 @@ describe('knockout.dragdrop', function () {
             el.dispatchEvent(createEvent('dragstart'));
         });
 
-        it('supports dragEnd event', function (done) {
+        it('supports dragEnd callback', function (done) {
             var el = document.getElementById('end-drag');
             ko.applyBindings({
                 dragEnd: function (data, event, zone) {
